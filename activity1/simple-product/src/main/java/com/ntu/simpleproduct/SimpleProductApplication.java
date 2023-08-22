6b69871a2a287d49709ea9269004c7adcc4a62d0
Justen Manni <me@justenmanni.dev>
2023-08-22 18:45:48 +0800

    Complete simple-product

--- activity1/simple-product/src/main/java/com/ntu/simpleproduct/SimpleProductApplication.java
+++ activity1/simple-product/src/main/java/com/ntu/simpleproduct/SimpleProductApplication.java
@@ -2,12 +2,31 @@ package com.ntu.simpleproduct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SimpleProductApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleProductApplication.class, args);
+	}
+
+	@GetMapping("/products")
+	public String product() {
+		return "This is the Product Page";
+	}
+
+	@GetMapping("/products/{productId}")
+	public String getProduct(@PathVariable int productId) {
+		return switch (productId) {
+			case 1 -> "This is the detail for ID " + productId + ": Apple";
+			case 2 -> "This is the detail for ID " + productId + ": Banana";
+			case 3 -> "This is the detail for ID " + productId + ": Carrot";
+			default -> "No product found";
+		};
 	}
 
 }

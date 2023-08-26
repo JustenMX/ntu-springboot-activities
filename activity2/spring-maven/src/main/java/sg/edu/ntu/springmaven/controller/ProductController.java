809b0b2acdf730b697c25e856fae71edd330d703
Justen Manni <me@justenmanni.dev>
2023-08-26 14:37:05 +0800

    Add ProductController and endpoints

--- activity2/spring-maven/src/main/java/sg/edu/ntu/springmaven/controller/ProductController.java
+++ activity2/spring-maven/src/main/java/sg/edu/ntu/springmaven/controller/ProductController.java
@@ -3,8 +3,12 @@ package sg.edu.ntu.springmaven.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +43,36 @@ public class ProductController {
     @GetMapping("/products/{id}")
     public Product getProduct(@PathVariable String id) {
         return productService.getProduct(id);
+    }
+
+    /*
+     * UPDATE
+     * -> PUT METHOD
+     */
+
+    @PutMapping("/products/{id}")
+    public Product updateProduct(@PathVariable String id, @RequestBody Product updatedProduct) {
+        return productService.updateProduct(updatedProduct);
+    }
+
+    /*
+     * CREATE
+     * -> POST METHOD
+     */
+
+    @PostMapping("/products")
+    public Product addProduct(@RequestBody Product product) {
+        return productService.addProduct(product);
+    }
+
+    /*
+     * DELETE
+     * -> DELETE METHOD
+     */
+
+    @DeleteMapping("/products/{id}")
+    public Product deleteProduct(@PathVariable String id) {
+        return productService.deleteProduct(id);
     }
 
 }
